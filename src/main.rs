6bf24cbb1d2ce4947504d6925400b80d95418924
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-06-26 14:24:31 +0900

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    {
+        let mut s1 = String::from("hello");
+        // s1はこのスコープで有効
+        println!("{}", s1);
+
+        let s2 = &mut s1; // s1の可変参照をs2に渡す
+
+        // s2はこのスコープで有効
+        s2.push_str(", world!");
+        println!("{}", s2);
+
+        println!("{}", s1); // s1はこのスコープで有効
+
+    }
 }
