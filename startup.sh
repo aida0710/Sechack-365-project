2dffd184f447da1ecd6ff6a2cb0fcced45c94975
aida0710 <51500566+aida0710@users.noreply.github.com>
2024-07-03 17:57:43 +0900

    起動用スクリプトを作成

--- startup.sh
+++ startup.sh
@@ -1,4 +1,25 @@
-sudo /home/aida/.cargo/bin/cargo run --package cli-app --bin cli-app
+#!/bin/bash
+
+# Rustのデフォルトツールチェーンを設定
+echo "Rustのデフォルトツールチェーンを安定版に設定します..."
+rustup default stable
+
+# プロジェクトディレクトリに移動
+echo "プロジェクトディレクトリに移動します..."
+
+# shellcheck disable=SC2164
+cd ~/RustroverProjects/rust-cli-app
+
+# 既存のビルドファイルを削除
+echo "既存のビルドファイルを削除します..."
+cargo clean
+
+# プロジェクトをビルド
+echo "プロジェクトをビルドします..."
 cargo build --release
+
+# 実行ファイルに権限を付与
 sudo setcap cap_net_raw,cap_net_admin=eip target/release/cli-app
-./target/release/cli-app
+
+echo "アプリケーションを実行します..."
+sudo ./target/release/cli-app
