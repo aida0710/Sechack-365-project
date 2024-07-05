d74113b72a44f21bd7be5508fb566ec315282047
aida0710 <51500566+aida0710@users.noreply.github.com>
2024-07-05 13:09:47 +0900

    キャプチャオプションを修正

--- src/packet_capture.rs
+++ src/packet_capture.rs
@@ -2,8 +2,10 @@ use pcap::{Capture, Device};
 
 pub fn packet_capture(capture_device: Device) -> Result<(), Box<dyn std::error::Error>> {
     let mut cap = Capture::from_device(capture_device)?
-        .promisc(true)
-        .snaplen(65535)
+        .promisc(true) // プロミスキャスモードを有効にする(自分宛以外のパケットもキャプチャする)
+        .snaplen(65535) // キャプチャするパケットの最大サイズを指定(65535バイトはEthernetフレームの最大サイズなので、実質無制限)
+        .buffer_size(5 * 1024 * 1024) // バッファサイズを5MBに指定
+        .immediate_mode(true) // キャプチャを開始するとすぐにパケットを取得する
         .open()?;
 
     let mut save_file = cap.savefile("capture.pcap")?;
